283f3cf9face9c9b0f017f136ef5d5ea8298b137
NormanSt98 <38924048+NormanSt98@users.noreply.github.com>
2021-12-05 20:29:30 +0100

    Clients update

--- Assets/Scripts/World/Characters/Behaviours/ClientNormalBehaviour.cs
+++ Assets/Scripts/World/Characters/Behaviours/ClientNormalBehaviour.cs
@@ -11,25 +11,28 @@ public class ClientNormalBehaviour : MonoBehaviour
     private StateMachineEngine m_SM;
 
     private Vector3 bakeryPosition;
+    private Vector3 counterPosition;
+    private Vector3 homePosition;
     private DayNightCycleComponent dayCicle;
+    private Singleton_Bakery_Queue bakeryQueue;
 
     private void Awake()
     {
         m_Actions = GetComponent<CharacterActions>();
         m_Blackboard = GetComponent<CharacterBlackboard>();
         m_Waypoints = GetComponent<CharacterWaypoints>();
-
-        
     }
 
     private void Start()
     {
+        dayCicle = ComponentRegistry.GetInst().GetSingletonComponent<DayNightCycleComponent>();
+        bakeryPosition = m_Waypoints.GetWaypointPosition(new ID("bakery"));
+        counterPosition = m_Waypoints.GetWaypointPosition(new ID("counter"));
+        homePosition = m_Waypoints.GetWaypointPosition(new ID("home"));
+        bakeryQueue = Singleton_Bakery_Queue.Instance;
+
         // Init State Machine
         CreateStateMachine();
-
-        bakeryPosition = m_Waypoints.GetWaypointPosition(new ID("bakery"));
-
-        dayCicle = ComponentRegistry.GetInst().GetSingletonComponent<DayNightCycleComponent>();
     }
 
     private void Update()
@@ -41,24 +44,36 @@ public class ClientNormalBehaviour : MonoBehaviour
     {
         m_SM = new StateMachineEngine(false);
 
-        State house = m_SM.CreateEntryState("atHouse", () => Debug.Log("Sleeping at home..."));
+        State home = m_SM.CreateEntryState("atHome", () => Debug.Log("Sleeping at home..."));
         State houseToBakery = m_SM.CreateState("moving to bakery", MoveToBakery);
+        State bakeryToHome = m_SM.CreateState("moving to home", MoveToHome);
         State bakery = m_SM.CreateState("atBakery", () => Debug.Log("Arrived at bakery!"));
+        State inQueue = m_SM.CreateState("in queue", EnterQueue);
+        State moveToCounter = m_SM.CreateState("moving to counter", MoveToCounter);
+        State atCounter = m_SM.CreateState("at counter", () => Debug.Log("Waiting at the counter..."));
 
         Perception itsBakeryHour = m_SM.CreatePerception<ValuePerception>(CheckTimeToBakery);
+
+        Perception canQueue = m_SM.CreatePerception<ValuePerception>(() => bakeryQueue.EnterQueue(gameObject));
+        Perception notQueue = m_SM.CreatePerception<ValuePerception>(() => !bakeryQueue.EnterQueue(gameObject));
 
         //Vector3 dest = m_Waypoints.GetWaypointPosition(new ID("bakeryTest"));
         //Perception arriveAtBakery = m_SM.CreatePerception<ValuePerception>( () => (dest.x) == transform.position.x && dest.z == transform.position.z);
         Perception arriveAtBakery = m_SM.CreatePerception<ValuePerception>(CheckArrivedAtBakery);
+        Perception arriveAtCounter = m_SM.CreatePerception<ValuePerception>(CheckArrivedAtCounter);
+        Perception arriveAtHome = m_SM.CreatePerception<ValuePerception>(CheckArrivedAtHome);
 
-        m_SM.CreateTransition("house to bakery", house, itsBakeryHour, houseToBakery);
-        m_SM.CreateTransition("bakery", houseToBakery, arriveAtBakery, bakery);
+        m_SM.CreateTransition("move to bakery", home, itsBakeryHour, houseToBakery);
+        m_SM.CreateTransition("arrive bakery", houseToBakery, arriveAtBakery, bakery);
+        //m_SM.CreateTransition("move to counter", bakery, notQueue, moveToCounter);
+        m_SM.CreateTransition("move to queue", bakery, canQueue, inQueue);
+        m_SM.CreateTransition("move home", bakery, notQueue, bakeryToHome);
+        m_SM.CreateTransition("arrive home", bakeryToHome, arriveAtHome, home);
     }
 
     private bool CheckTimeToBakery()
     {
-        //int hour = dayCicle.m_Hour;
-        int hour = ComponentRegistry.GetInst().GetSingletonComponent<DayNightCycleComponent>().m_Hour;
+        int hour = dayCicle.m_Hour;
         return hour >= 9 && hour <= 15;
     }
 
@@ -67,8 +82,34 @@ public class ClientNormalBehaviour : MonoBehaviour
         return transform.position.x == bakeryPosition.x && transform.position.z == bakeryPosition.z;
     }
 
+    private bool CheckArrivedAtHome()
+    {
+        return transform.position.x == homePosition.x && transform.position.z == homePosition.z;
+    }
+
+    private bool CheckArrivedAtCounter()
+    {
+        return transform.position.x == counterPosition.x && transform.position.z == counterPosition.z;
+    }
+
     private void MoveToBakery()
     {
-        m_Actions.MoveTo(m_Waypoints.GetWaypointPosition(new ID("bakery")));
+        m_Actions.MoveTo(bakeryPosition);
+    }
+
+    private void MoveToCounter()
+    {
+        m_Actions.MoveTo(counterPosition);
+    }
+
+    private void MoveToHome()
+    {
+        m_Actions.MoveTo(homePosition);
+    }
+
+    private void EnterQueue()
+    {
+        Debug.Log("Waiting in queue...");
+        //bakeryQueue.EnterQueue(gameObject);
     }
 }
